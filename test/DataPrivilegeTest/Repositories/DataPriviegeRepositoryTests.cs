36c231a5c7a81fb59ae4d57cec54ed91cf50c97d
carsonPan <carsonPan@outlook.com>
2020-05-13 21:53:43 +0800

    添加部分测试

---
+++ test/DataPrivilegeTest/Repositories/DataPriviegeRepositoryTests.cs
@@ -0,0 +1,91 @@
+﻿using DataPrivilege.Models;
+using DataPrivilege.Repositories;
+using Microsoft.EntityFrameworkCore;
+using Microsoft.Extensions.DependencyInjection;
+using Shouldly;
+using System;
+using System.Collections.Generic;
+using System.Text;
+using Xunit;
+
+namespace DataPrivilegeTest.Repositories
+{
+    public class DataPriviegeRepositoryTests
+    {
+        //public void Create_Rule_ReturnsSameRule()
+        //{
+        //    TestDbContext dbContext = CreateDbContext();
+        //    var repository = CteateRepository(dbContext);
+        //    DataPrivilegeRule dataPrivilegeRule = new DataPrivilegeRule() { Id = "0" };
+        //    dataPrivilegeRule= repository.Create(dataPrivilegeRule);
+        //    dataPrivilegeRule.Id.ShouldBe("0");
+        //}
+
+        //public void Create_Rule_RuleIsCreated()
+        //{
+        //    TestDbContext dbContext = CreateDbContext();
+        //    var repository = CteateRepository(dbContext);
+        //    DataPrivilegeRule dataPrivilegeRule = new DataPrivilegeRule() { Id = "0" };
+        //    repository.Create(dataPrivilegeRule);
+        //    dbContext = CreateDbContext();
+        //    var rule= dbContext.DataPrivilegeRules.Find("0");
+        //    rule.ShouldNotBeNull();
+        //}
+        [Fact]
+        public void Update_EntityIsNotFound_ThrowDbUpdateConcurrencyException()
+        {
+            TestDbContext dbContext = CreateDbContext();
+            var respository = CteateRepository(dbContext);
+            Should.Throw<DbUpdateConcurrencyException>(() => respository.Update(new DataPrivilegeRule() { Id = "1" }));
+        }
+        [Fact]
+        public void Update_EntityIsFound_EntityIsUpdated()
+        {
+            TestDbContext dbContext = CreateDbContext();
+            dbContext.DataPrivilegeRules.Add(new DataPrivilegeRule() { Id = "0", TableName = "c", ConditionExpression = "a=b" });
+            dbContext.SaveChanges();
+            dbContext = CreateDbContext();
+            var respository = CteateRepository(dbContext);
+            var rule = respository.Update(new DataPrivilegeRule() { Id = "0", Description = "hello world" });
+            var des = dbContext.DataPrivilegeRules.Find("0");
+            rule.Id.ShouldBe(des.Id);
+            rule.Description.ShouldBe(des.Description);
+            des.Description.ShouldBe("hello world");
+        }
+
+        [Fact]
+        public void Delete_EntityIsNotFound_ThrowDbUpdateConcurrencyException()
+        {
+            TestDbContext dbContext = CreateDbContext();
+            var respository = CteateRepository(dbContext);
+            Should.Throw<DbUpdateConcurrencyException>(() => respository.Delete(new DataPrivilegeRule() { Id = "1" }));
+
+        }
+
+        [Fact]
+        public void Delete_EntityIsFound_EntityIsDeleted()
+        {
+            TestDbContext dbContext = CreateDbContext();
+            dbContext.DataPrivilegeRules.Add(new DataPrivilegeRule() { Id = "1", TableName = "c", ConditionExpression = "a=b" });
+            dbContext.SaveChanges();
+            dbContext = CreateDbContext();
+            var respository = CteateRepository(dbContext);
+            respository.Delete(new DataPrivilegeRule() { Id = "1" });
+            var rule = dbContext.DataPrivilegeRules.Find("1");
+            rule.ShouldBeNull();
+
+        }
+
+        private TestDbContext CreateDbContext()
+        {
+            DbContextOptionsBuilder<TestDbContext> optionsBuilder = new DbContextOptionsBuilder<TestDbContext>();
+            optionsBuilder.UseInMemoryDatabase("test");
+            return new TestDbContext(optionsBuilder.Options);
+        }
+        private static DataPriviegeRepository<TestDbContext, DataPrivilegeRule, DataPrivilegeRuleRole<DataPrivilegeRule>, DataPrivilegeRuleUser<DataPrivilegeRule>> CteateRepository(TestDbContext dbContext)
+        {
+
+            return new DataPriviegeRepository<TestDbContext, DataPrivilegeRule, DataPrivilegeRuleRole<DataPrivilegeRule>, DataPrivilegeRuleUser<DataPrivilegeRule>>(dbContext);
+        }
+    }
+}
---
+++ test/DataPrivilegeTest/TestDbContext.cs
@@ -0,0 +1,78 @@
+﻿using DataPrivilege.Extensions;
+using DataPrivilege.Models;
+using Microsoft.EntityFrameworkCore;
+using System;
+using System.Collections.Generic;
+using System.Dynamic;
+using System.Text;
+
+namespace DataPrivilegeTest
+{
+    public class TestDbContext<TRule, TRuleRole, TRuleUser>:DbContext
+            where TRule : DataPrivilegeRule
+            where TRuleRole : DataPrivilegeRuleRole<TRule>
+            where TRuleUser : DataPrivilegeRuleUser<TRule>
+    {
+        public TestDbContext(DbContextOptions<TestDbContext> options)
+            :base(options)
+        { }
+        public DbSet<TRule> DataPrivilegeRules { get; set; }
+        public DbSet<TRuleRole> DataPrivilegeRuleRoles { get; set; }
+        public DbSet<TRuleUser> DataPrivilegeRuleUsers { get; set; }
+
+        public DbSet<TestEntity> TestEntities { get; set; }
+
+        protected override void OnModelCreating(ModelBuilder modelBuilder)
+        {
+            modelBuilder.ConfigureDataPrivilegeStore<TRule, TRuleRole, TRuleUser>();
+        }
+    }
+
+    public class TestDbContext : TestDbContext<DataPrivilegeRule, DataPrivilegeRuleRole<DataPrivilegeRule>, DataPrivilegeRuleUser<DataPrivilegeRule>>
+    {
+        public TestDbContext(DbContextOptions<TestDbContext> options) : base(options)
+        {
+        }
+    }
+
+    public class TestEntity
+    {
+        public string Id { get; set; }
+        public decimal DecimalProp { get; set; }
+        public float FloatProp { get; set; }
+        public long LongProp { get; set; }
+        public int IntProp { get; set; }
+        public short ShortProp { get; set; }
+        public byte ByteProp { get; set; }
+        public bool BoolProp { get; set; }
+        public decimal? NullableDecimalProp { get; set; }
+        public float? NullableFloatProp { get; set; }
+        public long? NullableLongProp { get; set; }
+        public int? NullableIntProp { get; set; }
+        public short? NullableShortProp { get; set; }
+        public byte? NullableByteProp { get; set; }
+        public bool? NullableBoolProp { get; set; }
+
+   
+        public TestRelation ShadowProp { get; set; }
+
+        //public ICollection<TestMultiple> TestMultiples { get; set; }
+
+    }
+
+    //public class TestMultiple
+    //{
+    //    public string Id { get; set; }
+    //    public string Name { get; set; }
+
+    //    public ICollection<TestEntity> TestEntities { get; set; }
+    //}
+
+    public class TestRelation
+    { 
+        public string Id { get; set; }
+        public string Name { get; set; }
+
+        public ICollection<TestEntity> TestEntities { get; set; }
+    }
+}
